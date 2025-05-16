e32a29a8c454ab8dd9704d0e9a2d2d891026d444
oscargallo <oscargallo_bass@outlook.es>
2025-05-16 13:46:39 -0600

    Add main rout

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from config import server
 from database import db
 
+#To use routes
+from routes.main_routes import main
+
 app = server()
+app.register_blueprint(main)
 
 if __name__ == "__main__":
     with app.app_context():
