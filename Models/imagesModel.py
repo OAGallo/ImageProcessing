c4f36d7fcad4d5c275d5ffb6de01a55af8644920
oscargallo <oscargallo_bass@outlook.es>
2025-06-12 23:05:22 -0600

    ImagesModel: Add table name

--- Models/imagesModel.py
+++ Models/imagesModel.py
@@ -2,6 +2,8 @@ from database import db
 from datetime import datetime
 
 class Images(db.Model):
+    __tablename__ = 'images' 
+    
     id = db.Column(db.Integer, primary_key = True)
     
     filename = db.Column(db.String(200), nullable = False)
